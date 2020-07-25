cdeef871f15b3674c7101ad09c1663f6d09bdba9
Aswani <achuaswani.g@gmail.com>
2020-07-25 10:23:51 -0700

    Initial Commit

---
+++ ShoppingCart/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShoppingCart
+//
+//  Created by Aswani G on 7/25/20.
+//  Copyright © 2020 pixycoders private limited. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
